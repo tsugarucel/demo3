45bd963ef571b935af5fbc11ef90cfafb9d0ee65
tsugarucel <tsugaru_cel@hotmail.com>
2013-11-15 19:16:53 -0500

    f

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'rails', '4.0.0'
 
 # Use sqlite3 as the database for Active Record
 group :development do
-gem 'sqlite3', '1.3.8'
+#gem 'sqlite3', '1.3.8'
 end
 
 # Use SCSS for stylesheets
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,11 @@ Demo3::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'app701627.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.smtp_settings = {enable_starttls_auto: true,address: 'smtp.gmail.com',port: 587,authentication: 'plain',user_name:'sucorreodegmail',password:'suclave'}
+
 end
